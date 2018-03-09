ba068bfd2a971035ea6359caebf5f2cef5c73c0e
Vinícius <carneiropresent@gmail.com>
2018-03-09 10:20:24 -0300

    Correcting spelling errors in the test.

--- tests/spec/js/cashSpec.js
+++ tests/spec/js/cashSpec.js
@@ -14,22 +14,22 @@ it("Error: Can't be empty.", function() {
 	expect(withdraw("")).toEqual("Empty Set");
 });
 
-it("Error: can't be negative.", function() {
+it("Error: Can't be negative.", function() {
 	expect(withdraw(-130)).toEqual("InvalidArgumentException");
 });
 
-it("Error: without valid notes.", function() {
+it("Error: Not valid notes.", function() {
 	expect(withdraw(125)).toEqual("NoteUnavailableException");
 });
 
-it("Error: without valid notes.", function() {
+it("Error: User try to type a not valid character.", function() {
 	expect(withdraw("a")).toEqual("Only numbers please");
 });
 
-it("Expect to: without 30 notes [20,10].", function() {
+it("Expect to: Withdraw 30 notes [20,10].", function() {
 	expect(withdraw(30)).toEqual([20,10]);
 });
 
-it("Expect to: without notes [50,20,10].", function() {
+it("Expect to: Withdraw notes [50,20,10].", function() {
 	expect(withdraw(80)).toEqual([50,20,10]);
 });
